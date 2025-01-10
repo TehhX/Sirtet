c7d2cbec9dc2d453911e46de3e3cc313a7debb53
Sam <156055395+TehhX@users.noreply.github.com>
2025-01-10 01:19:31 -0500

    Update VolumeSliders.java

--- src/VolumeSliders.java
+++ src/VolumeSliders.java
@@ -8,12 +8,14 @@ import java.util.ArrayList;
  * are used by multiple other classes, it made sense to make them their own class to have
  * it editable all in the same place. */
 class VolumeSliders extends SirtetPanel implements ChangeListener {
+    private static ArrayList<VolumeSliders> slidersArrayList = new ArrayList<>();
+
     private JSlider bgmSlider = sliderSetup();
     private JSlider sfxSlider = sliderSetup();
 
     public VolumeSliders() {
         super(false);
-        VolumeSlidersMaster.addSliderObject(this);
+        slidersArrayList.add(this);
 
         bgmSlider.setBounds(191, 720, 100, 50);
         bgmSlider.setValue(SaveData.bgmVolume);
@@ -44,29 +46,14 @@ class VolumeSliders extends SirtetPanel implements ChangeListener {
         else if (e.getSource() == sfxSlider)
             SaveData.sfxVolume = sfxSlider.getValue();
 
-        VolumeSlidersMaster.updateAll();
-
+        updateAllSliders();
         SirtetWindow.frame.requestFocus();
     }
 
-    public void updateSliders() {
-        bgmSlider.setValue(SaveData.bgmVolume);
-        sfxSlider.setValue(SaveData.sfxVolume);
-    }
-}
-
-/**
- * This class exists simply to update the values of all volume sliders objects when a value is changed in only one
- * of them. Without this, the values would be inconsistent between different VolumeSlider objects. */
-class VolumeSlidersMaster {
-    private static ArrayList<VolumeSliders> slidersArray = new ArrayList<>();
-
-    public static void addSliderObject(VolumeSliders slidersObject) {
-        slidersArray.add(slidersObject);
-    }
-
-    public static void updateAll() {
-        for (VolumeSliders sliders : slidersArray)
-            sliders.updateSliders();
+    public static void updateAllSliders() {
+        for (VolumeSliders sliders : slidersArrayList) {
+            sliders.bgmSlider.setValue(SaveData.bgmVolume);
+            sliders.sfxSlider.setValue(SaveData.sfxVolume);
+        }
     }
 }
